c6c62b3b8b8d6747fa7d1f1fb6499725028d5606
Adam Zucker <adamzu@gmail.com>
2017-02-16 13:59:36 -0500

    untracked pycache and venv

--- project/application.py
+++ project/application.py
@@ -1,14 +1,5 @@
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-
-# @app.after_request
-# def force_cache_flush(r):
-#     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-#     r.headers["Pragma"] = "no-cache"
-#     r.headers["Expires"] = "0"
-#     r.headers['Cache-Control'] = 'public, max-age=0'
-#     return r
 
 @app.route("/")
 def index():
