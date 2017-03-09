4ae131215d0a404c5128a091d49c6ea050e2e778
adamzu <adamzu@gmail.com>
2017-03-09 14:18:27 -0500

    added react-dropzone

--- project/static/js/Home.react.js
+++ project/static/js/Home.react.js
@@ -1,8 +1,14 @@
-import React from 'react';
+const React = require('react');
+const Dropzone = require('react-dropzone');
 
-var Home = React.createClass({
+const Home = React.createClass({
   render() {
-    return <h1 className="home">Image Caption Generation</h1>;
+    return (
+      <div>
+        <h1 className="home">Image Caption Generation</h1>
+        <Dropzone />
+      </div>
+    );
   }
 });
 module.exports = Home
--- project/static/js/application.js
+++ project/static/js/application.js
@@ -1,5 +1,8 @@
-import Home from './Home.react';
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import Home from './Home.react';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
+const React = require('react');
+const ReactDOM = require('react-dom');
+const Home = require('./Home.react');
 ReactDOM.render(<Home />, document.getElementById('content'));
