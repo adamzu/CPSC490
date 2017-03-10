8764dd05e3a5f3ab14c77598a053eaabda7aedf1
adamzu <adamzu@gmail.com>
2017-03-09 20:28:06 -0500

    playing around with Dropzone

--- project/static/js/Home.react.js
+++ project/static/js/Home.react.js
@@ -3,17 +3,34 @@ const Dropzone = require('react-dropzone');
 
 import { Button } from 'react-bootstrap';
 
-const Home = React.createClass({
+class Home extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      images: [],
+    }
+  }
+
+  onDrop(image) {
+    this.setState({
+      files: image
+    });
+  }
+
   render() {
     return (
       <div>
         <h1 className="home">Image Caption Generation</h1>
-        <Dropzone accept="image/jpg">
+        <Dropzone
+          accept="image/jpg"
+          onDrop={this.onDrop}
+          ref="dropzone">
           {/* <img src=""/> */}
         </Dropzone>
         <Button> Get Caption </Button>
       </div>
     );
   }
-});
+}
 module.exports = Home
