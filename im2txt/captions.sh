e32ea55936bd458ecbee20ef3c1d8f7b3cefede2
adamzu <adamzu@gmail.com>
2017-04-01 23:16:56 -0400

    added im2txt cleanly

---
+++ im2txt/captions.sh
@@ -0,0 +1,13 @@
+MODEL_PATH="${PWD}/im2txt/model"
+CHECKPOINT_PATH="${MODEL_PATH}/model.ckpt-2000000"
+VOCAB_FILE="${MODEL_PATH}/word_counts.txt"
+IMAGE_FILE="${PWD}/im2txt/test.jpg"
+
+# Build the inference binary.
+bazel build -c opt im2txt/run_inference
+
+# Run inference to generate captions.
+bazel-bin/im2txt/run_inference \
+  --checkpoint_path=${CHECKPOINT_PATH} \
+  --vocab_file=${VOCAB_FILE} \
+  --input_files=${IMAGE_FILE}
