d4901181dab0653baaea6809c24cf965738977dd
Adam Zucker <adamzu@gmail.com>
2017-04-20 00:14:40 -0400

    installed sockets

--- project/application.py
+++ project/application.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template, request, session
 from flask_session import Session
+from flask_socketio import SocketIO, emit
 from tempfile import gettempdir
 from ProcessedImage import InvalidImageException, ProcessedImage
 
@@ -8,6 +9,7 @@ app.config['SESSION_FILE_DIR'] = gettempdir()
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
@@ -37,4 +39,4 @@ def caption():
     return caption
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
